1f08a88effe76ff9ea5b1a69182d394af149201b
JoeySpronck <joeyspronck67@hotmail.com>
2024-06-04 16:54:08 +0200

    convert_path correction

--- convert_path.py
+++ convert_path.py
@@ -4,11 +4,12 @@ current_os = "w" if os.name == "nt" else "l"
 
 def convert_path(path, to=current_os):
     if to in ["w", "windows"]:
-        path = path.replace("/mnt/pa_cpg", "Y:")
         path = path.replace("/data/pathology", "Z:")
         path = path.replace("/mnt/pa_cpgarchive1", "W:")
         path = path.replace("/mnt/pa_cpgarchive2", "X:")
+        path = path.replace("/mnt/pa_cpg", "Y:")
         path = path.replace("/", "\\")
+        
     if to in ["u", "unix", "l", "linux", 'm', 'mac']:
         path = path.replace("W:", "/mnt/pa_cpgarchive1")
         path = path.replace("X:", "/mnt/pa_cpgarchive2")
