f674a34a9dc4e78562ee97847fd581ce563b3f5b
JoeySpronck <joeyspronck67@hotmail.com>
2024-09-10 18:53:22 +0200

    updated convert path function

--- convert_path.py
+++ convert_path.py
@@ -3,17 +3,27 @@ import os
 current_os = "w" if os.name == "nt" else "l"
 
 def convert_path(path, to=current_os):
-    if to in ["w", "windows"]:
+    '''
+    This function converts paths to the format of the desired platform. 
+    By default it changes paths to the platform you are currently using.
+    This way you do not need to change your paths if you are executing your code on a different platform.
+    It may however be that you mounted the drives differently. 
+    In that case you may need to change that in the code below. 
+    For example, change the Z for blissey (where I mounted it) to X (how you may have mounted it)
+    If you add a linux path on a linux platform, or windows on windows, this function by default doesnt do anything.
+    '''
+    if to in ["w", "win", "windows"]:
         path = path.replace("/data/pathology", "Z:")
-        path = path.replace("/mnt/pa_cpgarchive1", "W:")
-        path = path.replace("/mnt/pa_cpgarchive2", "X:")
-        path = path.replace("/mnt/pa_cpg", "Y:")
+        path = path.replace("/data/pa_cpgarchive1", "W:")
+        path = path.replace("/data/pa_cpgarchive2", "X:")
+        path = path.replace("/data/pa_cpg", "Y:")
+        path = path.replace("/data/temporary", "T:")
         path = path.replace("/", "\\")
-        
-    if to in ["u", "unix", "l", "linux", 'm', 'mac']:
-        path = path.replace("W:", "/mnt/pa_cpgarchive1")
-        path = path.replace("X:", "/mnt/pa_cpgarchive2")
-        path = path.replace("Y:", "/mnt/pa_cpg")
+    if to in ["u", "unix", "l", "linux"]:
         path = path.replace("Z:", "/data/pathology")
+        path = path.replace("W:", "/data/pa_cpgarchive1")
+        path = path.replace("X:", "/data/pa_cpgarchive2")
+        path = path.replace("Y:", "/data/pa_cpg")
+        path = path.replace("T:", "/data/temporary")
         path = path.replace("\\", "/")
     return path
